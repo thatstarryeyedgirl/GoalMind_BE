1dcb23b80825c36792467d12ca83a2cbeacf6323
Valerie <okorovaleriechikanyima@gmail.com>
2025-11-05 13:58:03 +0100

    Updated goalmind_app/urls.py

--- goalmind_app/urls.py
+++ goalmind_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import TelexAgentAPIView, GoalAgentAPIView
 
 urlpatterns = [
-    
+    path('goal/', GoalAgentAPIView.as_view(), name='goal_agent'),
+    path('agent/', TelexAgentAPIView.as_view(), name='telex_workflow'),
 ]
