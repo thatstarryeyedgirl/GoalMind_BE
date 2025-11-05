52b929c1fdf8858f9296c47baa72b0d07511b531
Valerie <okorovaleriechikanyima@gmail.com>
2025-11-05 13:57:10 +0100

    Updated goalmind urls.py

--- goalmind/urls.py
+++ goalmind/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('a2a/agent/', include('goalmind_app.urls'))
+    path('a2a/', include('goalmind_app.urls'))
 ]
