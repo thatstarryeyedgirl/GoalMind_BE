8dbbcb6203c0f3ed72874e2bf5be52e7b52f2251
Valerie <okorovaleriechikanyima@gmail.com>
2025-11-06 16:28:03 +0100

    Preparing project for deployment on vercel

--- goalmind/settings.py
+++ goalmind/settings.py
@@ -78,8 +78,20 @@ WSGI_APPLICATION = 'goalmind.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': os.getenv('DB_SSLMODE'),
+            'channel_binding': os.getenv('DB_CHANNEL_BINDING'),
+        },
+    }
 }
 
 
