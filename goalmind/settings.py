b50efa2100218a1a0bb85098d33e0fecc7257823
Valerie <okorovaleriechikanyima@gmail.com>
2025-11-06 15:40:44 +0100

    Preparing project for deployment on vercel

--- goalmind/settings.py
+++ goalmind/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,18 +79,7 @@ WSGI_APPLICATION = 'goalmind.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE':os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'SSLMODE': os.getenv('DB_SSLMODE'),
-            'CHANNEL_BINDING': os.getenv('DB_CHANNEL_BINDING'),
-        }
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
