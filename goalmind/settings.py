bafa6f28e96911cce3a2d08e7cc279b382dcf6b3
Valerie <okorovaleriechikanyima@gmail.com>
2025-11-07 14:42:01 +0100

    Preparing project for deployment on vercel

--- goalmind/settings.py
+++ goalmind/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'goalmind.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('POSTGRES_URL'),
         conn_max_age=600,
         ssl_require=True
     )
