1c1d69029ba81fe65354d243d0341a686d9b6269
Valerie <okorovaleriechikanyima@gmail.com>
2025-11-07 11:02:58 +0100

    Preparing project for deployment on vercel

--- build_files.sh
+++ build_files.sh
@@ -5,7 +5,7 @@ echo "Building the project..."
 python3.12 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
+python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput
 
 echo "Collect Static..."
