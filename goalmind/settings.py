4d479f9975d8ae73feb21614df4ab78676ff7891
Valerie <okorovaleriechikanyima@gmail.com>
2025-11-07 11:13:34 +0100

    Preparing project for deployment on vercel

--- goalmind/settings.py
+++ goalmind/settings.py
@@ -25,9 +25,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')=='True'
+DEBUG = os.getenv('DEBUG', 'False')=='True'
 
-ALLOWED_HOSTS = ['*', 'goal-mind.vercel.app', '.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['goal-mind.vercel.app', '.vercel.app', '127.0.0.1']
 
 
 # Application definition
