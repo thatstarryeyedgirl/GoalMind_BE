8570aa99c95b33e2236af40420bc129fcee3cd07
Valerie <okorovaleriechikanyima@gmail.com>
2025-11-10 15:15:45 +0100

    Preparing project for deployment on python anywhere

--- goalmind/settings.py
+++ goalmind/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'goalmind.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('POSTGRES_DATABASE'),
         'USER': os.environ.get('POSTGRES_USER'),   
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': '5432',
+        'PORT': os.environ.get('POSTGRES_PORT'),
 }
 }
 
